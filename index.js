2df9f3eabb68c995f67d20c489260b1af3025242
Jonathan Pucm <jonathancoder77@outlook.com>
2017-10-23 13:01:30 +1100

    implement mongodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -9,3 +12,4 @@ require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
