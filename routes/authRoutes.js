6c3094b77a0cd95b3cee5d618399c245c4a7a571
Jonathan Pucm <jonathancoder77@outlook.com>
2017-10-29 20:02:35 +1100

    add logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,7 +16,14 @@ module.exports = (app) => {
 
 	app.get('/auth/facebook/callback', passport.authenticate('facebook'));
 
+
 	app.get('/api/current_user', (req, res) => {
 		res.send(req.user);
 	})
+
+	// Sign out
+	app.get('/api/logout', (req, res) => {
+	  req.logout();
+	  res.redirect('/');
+	});
 };
