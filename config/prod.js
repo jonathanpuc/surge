2492630aefaedb0b98ccc647bb48581f8da506e4
Jonathan Pucm <jonathancoder77@outlook.com>
2017-10-29 20:38:52 +1100

    build prod version for heroku

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+// dev.js dont commit this!
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	facebookAppId: process.env.FACEBOOK_APP_ID,
+	facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
