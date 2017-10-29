17c65ebb2c13004c09c7a0063b1e10316d176b15
Jonathan Pucm <jonathancoder77@outlook.com>
2017-10-29 18:44:32 +1100

    complete facebook oauth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,18 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
+	// Google Auth
 	app.get('/auth/google', passport.authenticate('google', {
 		scope: ['profile', 'email']
 	}));
 
 	app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+	// Facebook Auth
+	app.get('/auth/facebook', passport.authenticate('facebook'));
+
+	app.get('/auth/facebook/callback', passport.authenticate('facebook'));
 
 	app.get('/api/current_user', (req, res) => {
 		res.send(req.user);
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const keys = require('../config/keys');
 
@@ -30,7 +31,27 @@ passport.use(new GoogleStrategy({
 			} else {
 				// make a new record, when finished return no error and new created user
 				new User({ googleId: profile.id }).save().then(user => done(null, user));
+			}
+		});
 
+}));
+
+
+passport.use(new FacebookStrategy({
+    clientID: keys.facebookAppId,
+    clientSecret: keys.facebookAppSecret,
+    callbackURL: '/auth/facebook/callback',
+    profileFields: ['id']
+}, (accessToken, refreshToken, profile, done) => {
+	
+	User.findOne({ facebookId: profile.id }).then(existingUser => {
+			if (existingUser) {
+				// already have a record with given profile id
+				// tell passport we finished, no error and here's the existing user
+				done(null, existingUser);
+			} else {
+				// make a new record, when finished return no error and new created user
+				new User({ facebookId: profile.id }).save().then(user => done(null, user));
 			}
 		});
 
