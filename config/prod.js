5e64378807da70b5eb58bcf32436b7ff5f43dce3
Jonathan Pucm <jonathancoder77@outlook.com>
2017-10-31 13:46:55 +1100

    install stripe react, add env variables

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
 	facebookAppId: process.env.FACEBOOK_APP_ID,
 	facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	cookieKey: process.env.COOKIE_KEY
+	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
